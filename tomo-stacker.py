0080a3e8c25a601acdeb9a2266b7c17bfb4f43dd
fbscem <fbscem@absl-wkstn04.leeds.ac.uk>
2019-04-12 16:03:51 +0100

    fixed tilt angles in proper format

--- tomo-stacker.py
+++ tomo-stacker.py
@@ -37,9 +37,9 @@ def parse_filename(infile):
 def update_header(infile,tilt,tilt_axis,apix):
 	'''update the header of a single mrc file using imod alterheader. Add tilt angle, tilt axis, and update pixel size'''
 	print('{0}\t{1}'.format(tilt,infile))
-	subprocess.call(['alterheader','{0}'.format(infile),'-tlt','0,0,{0}'.format(tilt),'-del','{0},{0},{0}'.format(apix),'-title','Tilt axis rotation angle = {0}'.format(tilt_axis)],stdout=logout)
+	subprocess.call(['alterheader','{0}'.format(infile),'-del','{0},{0},{0}'.format(apix),'-title','Tilt axis rotation angle = {0}'.format(tilt_axis)],stdout=logout)
 
-def put_in_dict(file,shortname,tilt):
+def put_in_dict(file,shortname):
 	'''Put the files in a dictionary by short name (which is the name of the final tomogram). Returns dictionary: {tomo name:(tilt,full filename),(tilt 2,full filename 2)...(tilt n,full filename n)}'''
 	try:
 		filesdic[shortname].append((tilt,file))
@@ -58,18 +58,22 @@ print('tilt\tfile name')					# screen output
 for i in sys.argv[1:-2]:					# operate on all files 1 at a time
 	shortname,tilt = parse_filename(i)				# get the name and tilt for the file
 	update_header(i,tilt,tilt_axis,apix)				# update the file's header
-	put_in_dict(i,shortname,tilt)					# put the file in the dictionary attached to the correct tomo
+	put_in_dict(i,shortname)					# put the file in the dictionary attached to the correct tomo
 print(':: making stacks ::')					# screen output
 print('#images\tname')						# screen output
 finished_files = []						# make a list of finished files **
 for i in filesdic:						# operate on each tomogram
 	print("{0}\t{1}/{1}.mrc".format(len(filesdic[i]),i))		# screen output
 	if os.path.isdir(i) == False:					# check that the output directory exists
-		subprocess.call(['mkdir',i])					# if it doesn't create it
-	sortedlist = ['newstack']					# start building the command that will  go into newstack
+		subprocess.call(['mkdir',i])				# if it doesn't create it
 	files_sorted = sorted(filesdic[i], key=getkey)			# make a list of the subfiles in the tomogram in order by tilt
+	tiltsfile = open('{0}/{0}_tilts.txt'.format(i),'w')			# open a file to write the tilts to
 	for j in files_sorted:						# for every file in that list
-		sortedlist.append(j[1])						# add the filename to the newstack command 
+		tiltsfile.write('{0}\n'.format(j[0]))			# add the tilt to te tilt file			
+	tiltsfile.close()						# close the tilts file
+	sortedlist = ['newstack','-tilt','{0}/{0}_tilts.txt'.format(i)]					# start building the command that will  go into newstack
+	for j in files_sorted:						# for every file in that list
+		sortedlist.append(j[1])					# add the filename to the newstack command 
 	sortedlist.append('{0}/{0}.mrc'.format(i))			# add the output file name to the end of the newstack command
 	subprocess.call(sortedlist,stdout=logout)			# run the newstack command, write newstack's output to the logfile
 	finished_files.append('{0}/{0}.mrc'.format(i))			# add the file to the finished list **
